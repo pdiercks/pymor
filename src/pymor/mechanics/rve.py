787da7ca9b624112dbd657f12780b52b5bc2ccdd
pdiercks <philipp.diercks@bam.de>
2019-10-18 18:07:06 +0200

    [mechanics.rve] debug get_linear_elastic_macro_strain_operator

--- src/pymor/mechanics/rve.py
+++ src/pymor/mechanics/rve.py
@@ -7,6 +7,7 @@ from pymor.core.config import config
 if config.HAVE_FENICS:
     import ufl
     import dolfin as df
+    import numpy as np
     from pymor.operators.constructions import LincombOperator, VectorOperator
     from pymor.parameters.base import ParameterType
     from pymor.parameters.functionals import (
@@ -15,7 +16,7 @@ if config.HAVE_FENICS:
         ProductParameterFunctional
     )
 
-    def get_linear_elastic_macro_strain_operator(range_space, measure):
+    def get_linear_elastic_macro_strain_operator(range_space, parameter_type, measure, subdomain_ids):
         r"""Returns a |LincombOperator| which represents the parameter separated form of
 
         .. math::
@@ -24,49 +25,26 @@ if config.HAVE_FENICS:
         where the 6 independent components of the macroscopic strain :math:`E`
         and young's modulus and poisson ratio of the material are used as parameters.
         """
+        assert isinstance(subdomain_ids, tuple)
+        assert subdomain_ids[0] > 0, "marking should start from 1, which is usually the case for gmsh-generated meshes"
 
-        family = range_space.V.ufl_element(),family()
+        NOMEGA = len(subdomain_ids)
+        shapes = {'EPS': ((3,), (6,)), 'E': ((NOMEGA,),), 'NU': ((NOMEGA,),)}
+
+        family = range_space.V.ufl_element().family()
         assert family in ('Lagrange', 'Discontinuous Lagrange', 'Mixed')
-        assert isinstance(measure, ufl.measure.Measure)
-
+        if family == 'Mixed':
+            v_test, lambda_v = df.TestFunctions(range_space.V)
+        else:
+            v_test = df.TestFunction(range_space.V)
         gdim = range_space.V.mesh().geometric_dimension()
         assert gdim in (2, 3)
-        ncomponents = 3
-        if gdim == 3:
-            ncomponents *= 2
 
-        pt = ParameterType({'EPS': (ncomponents,), 'E': (), 'NU': ()})
-
-        # ### Lame constants as parameter functionals
-        L = ExpressionParameterFunctional("E * NU / ((1.0 + NU) * (1.0 - 2.0 * NU))", pt) # lambda
-        M = ExpressionParameterFunctional("E / 2.0 / (1.0 + NU)", pt)                     # mu
-
-        # ### trace of E as parameter functional
-        expression = ' + '.join([f'EPS[{i}]' for i in range(gdim)])
-        trE = ExpressionParameterFunctional(expression, pt)
-
-        # ### parameter functional for 1st term
-        LtrE = ProductParameterFunctional([L, trE])
-
-        # ### µ_ij as parameter functionals
-        mu_ij = [ProjectionParameterFunctional(component_name='EPS',
-                                               component_shape=(ncomponents,),
-                                               coordinates=(c,))
-                 for c in range(ncomponents)]
-
-        # ### list of (product) parameter functionals for 6 forms resulting from 2nd term
-        M_mu_ij = [ProductParameterFunctional([M, mu]) for mu in mu_ij]
-
-        # ### gather all parameter functionals
-        thetas = [LtrE] + M_mu_ij
-
-        # ### assemble vectors
-        if family == 'Mixed':
-            v, lambda_v = df.TestFunctions(range_space.V)
-        else:
-            v = df.TestFunction(range_space.V)
-        W = df.sym(df.grad(v))
-        vector = []
+        pt = parameter_type
+        for k in pt.keys():
+            assert k in ('EPS', 'E', 'NU')
+            assert pt[k] in shapes[k]
+        assert isinstance(measure, ufl.measure.Measure)
 
         # ### caution! the following (voigt) mapping is used
         #  in the 2d case:
@@ -78,15 +56,47 @@ if config.HAVE_FENICS:
         #          [EPS[3], EPS[1], EPS[5]],
         #          [EPS[4], EPS[5], EPS[2]]]
 
-        for i in range(gdim):
-            vector.append(df.assemble(2.0 * W[i, i] * measure))
+        vector = []
+        thetas = []
 
-        for i in range(gdim):
-            for j in range(gdim):
-                if i < j:
-                    vector.append(df.assemble(4.0 * W[i, j] * measure))
+        for ID in subdomain_ids:
+            # ### Lame constants as parameter functionals
+            L = ExpressionParameterFunctional(
+                f"E[{int(ID-1)}] * NU[{int(ID-1)}] / ((1.0 + NU[{int(ID-1)}]) * (1.0 - 2.0 * NU[{int(ID-1)}]))", pt) # lambda
+            M = ExpressionParameterFunctional(f"E[{int(ID-1)}] / 2.0 / (1.0 + NU[{int(ID-1)}])", pt)       # mu
 
-        vector_operators = [VectorOperator(FenicsVectorSpace(range_space.V).make_array([v])) for v
-                            in vector]
+            # ### trace of E as parameter functional
+            expression = ' + '.join([f'EPS[{i}]' for i in range(gdim)])
+            trE = ExpressionParameterFunctional(expression, pt)
+
+            # ### parameter functional for 1st term
+            LtrE = ProductParameterFunctional([L, trE])
+
+            # ### µ_ij as parameter functionals
+            mu_ij = [ProjectionParameterFunctional(component_name='EPS',
+                                                   component_shape=(pt['EPS'][0],),
+                                                   coordinates=(c,))
+                     for c in range(pt['EPS'][0])]
+
+            # ### list of (product) parameter functionals for 6 forms resulting from 2nd term
+            M_mu_ij = [ProductParameterFunctional([M, mu]) for mu in mu_ij]
+
+            # ### gather all parameter functionals
+            thetas += [LtrE] + M_mu_ij
+
+            # ### assemble vectors
+            W = df.sym(df.grad(v_test))
+            vector.append(df.assemble(df.tr(W) * measure(ID)))
+
+            for i in range(gdim):
+                vector.append(df.assemble(2.0 * W[i, i] * measure(ID)))
+
+            for i in range(gdim):
+                for j in range(gdim):
+                    if i < j:
+                        vector.append(df.assemble(4.0 * W[i, j] * measure(ID)))
+
+        # turn all vectors into VectorOperators
+        vector_operators = [VectorOperator(range_space.make_array([v])) for v in vector]
 
         return LincombOperator(vector_operators, thetas)
