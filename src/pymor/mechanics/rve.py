797acd85577edd80ee3916c48b80a2bd5f33c620
pdiercks <philipp.diercks@bam.de>
2019-10-29 15:06:21 +0100

    [mechanics.rve] add helper f to get vector operators

--- src/pymor/mechanics/rve.py
+++ src/pymor/mechanics/rve.py
@@ -7,9 +7,7 @@ from pymor.core.config import config
 if config.HAVE_FENICS:
     import ufl
     import dolfin as df
-    import numpy as np
     from pymor.operators.constructions import LincombOperator, VectorOperator
-    from pymor.parameters.base import ParameterType
     from pymor.parameters.functionals import (
         ProjectionParameterFunctional,
         ExpressionParameterFunctional,
@@ -20,10 +18,13 @@ if config.HAVE_FENICS:
         r"""Returns a |LincombOperator| which represents the parameter separated form of
 
         .. math::
-            \int_{\Omega} \varepsilon(v) \cdot C \cdot E \; \mathrm{d}x,
+            \int_{\Omega} \varepsilon(v) \cdot C \cdot E(\mu) \; \mathrm{d}x, \quad \mathrm{with} \;
+            C = \lambda^1(\mu) \mathbf{1} \otimes \mathbf{1} + 2 \lambda^2(\mu)
+            \overset{<4>}{\mathbf{1}}
 
-        where the 6 independent components of the macroscopic strain :math:`E`
-        and young's modulus and poisson ratio of the material are used as parameters.
+        and the 6 independent components of the macroscopic strain :math:`E(\mu)`
+        and young's modulus and poisson ratio of the material are used as parameters, such that
+        Lame's constants are parameter fuctionals.
         """
         assert isinstance(subdomain_ids, tuple)
         assert subdomain_ids[0] > 0, "marking should start from 1, which is usually the case for gmsh-generated meshes"
@@ -62,7 +63,8 @@ if config.HAVE_FENICS:
         for ID in subdomain_ids:
             # ### Lame constants as parameter functionals
             L = ExpressionParameterFunctional(
-                f"E[{int(ID-1)}] * NU[{int(ID-1)}] / ((1.0 + NU[{int(ID-1)}]) * (1.0 - 2.0 * NU[{int(ID-1)}]))", pt) # lambda
+                f"E[{int(ID-1)}] * NU[{int(ID-1)}] / ((1.0 + NU[{int(ID-1)}]) * (1.0 - 2.0 * NU[{int(ID-1)}]))",
+                pt)  # lambda
             M = ExpressionParameterFunctional(f"E[{int(ID-1)}] / 2.0 / (1.0 + NU[{int(ID-1)}])", pt)       # mu
 
             # ### trace of E as parameter functional
@@ -100,3 +102,48 @@ if config.HAVE_FENICS:
         vector_operators = [VectorOperator(range_space.make_array([v])) for v in vector]
 
         return LincombOperator(vector_operators, thetas)
+
+    def get_macro_strain_vector_operators(range_space, measure, subdomain_id):
+        r"""Returns a list of |VectorOperator| representing the forms
+
+        .. math::
+            &\int_{\Omega} (v_{11} + v_{22} + v_{33}) \; \mathrm{d}x\\
+            2&\int_{\Omega} v_{kk} \; \mathrm{d}x, \quad \mathrm{for} \; k=1, 2, 3\\
+            4&\int_{\Omega} v_{ij} \; \mathrm{d}x, \quad \mathrm{for} \; (i,j)=(1,2),(1,3),(2,3)
+
+        """
+        assert isinstance(subdomain_id, int)
+        assert subdomain_id > 0, "marking should start from 1, which is usually the case for gmsh-generated meshes"
+        family = range_space.V.ufl_element().family()
+        assert family in ('Lagrange', 'Discontinuous Lagrange', 'Mixed')
+        if family == 'Mixed':
+            v_test, lambda_v = df.TestFunctions(range_space.V)
+        else:
+            v_test = df.TestFunction(range_space.V)
+        gdim = range_space.V.mesh().geometric_dimension()
+        assert gdim in (2, 3)
+
+        # ### caution! the following (voigt) mapping is used
+        #  in the 2d case:
+        #  E_ij = [[EPS[0], EPS[2], 0.0],
+        #          [EPS[2], EPS[1], 0.0],
+        #          [0.0, 0.0, 0.0]]
+        #  in the 3d case:
+        #  E_ij = [[EPS[0], EPS[3], EPS[4]],
+        #          [EPS[3], EPS[1], EPS[5]],
+        #          [EPS[4], EPS[5], EPS[2]]]
+        vector = []
+        W = df.sym(df.grad(v_test))
+        vector.append(df.assemble(df.tr(W) * measure(subdomain_id)))
+
+        for i in range(gdim):
+            vector.append(df.assemble(2.0 * W[i, i] * measure(subdomain_id)))
+
+        for i in range(gdim):
+            for j in range(gdim):
+                if i < j:
+                    vector.append(df.assemble(4.0 * W[i, j] * measure(subdomain_id)))
+
+        # turn all vectors into VectorOperators
+        vector_operators = [VectorOperator(range_space.make_array([v])) for v in vector]
+        return vector_operators
